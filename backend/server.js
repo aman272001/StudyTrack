6a0d620145ee6f3258b75cb15d60891358e2b577
aman272001 <amanraj27884@gmail.com>
2026-09-06 18:23:35 +0530

    new

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ connectDB();
 
 const app = express();
 
-app.use(cors({ origin: process.env.CLIENT_URL || "http://localhost:5173" }));
+app.use(cors({ origin: process.env.CLIENT_URL || "https://study-track-lac.vercel.app/" }));
 app.use(express.json({ limit: "3mb" }));
 app.use(express.urlencoded({ extended: false }));
 
