12c83f599bed2319abe456477c6bc2519ad3a353
aman272001 <amanraj27884@gmail.com>
2026-09-06 19:15:13 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -11,12 +11,8 @@ connectDB();
 
 const app = express();
 
-app.use(cors({ origin: process.env.CLIENT_URL || "https://study-track-lac.vercel.app/" }));
-// app.use(cors({
-//     origin: process.env.CLIENT_URL,
-//     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-//     allowedHeaders: ["Content-Type", "Authorization"]
-// }));
+app.use(cors({ origin: process.env.CLIENT_URL || "https://study-track-lac.vercel.app" }));
+
 
 app.options("*", cors());
 app.use(express.json({ limit: "3mb" }));
